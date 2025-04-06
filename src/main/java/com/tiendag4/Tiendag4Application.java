b5417390730822c8fd90c2dfe3c2c1f268cf8cac
Eddier511 <124939498+Eddier511@users.noreply.github.com>
2025-04-06 17:38:04 -0600

    Semana 11 Trabajo Final

---
+++ src/main/java/com/tiendag4/Tiendag4Application.java
@@ -0,0 +1,13 @@
+package com.tiendag4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiendag4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tiendag4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendag4/Tiendag4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendag4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tiendag4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
