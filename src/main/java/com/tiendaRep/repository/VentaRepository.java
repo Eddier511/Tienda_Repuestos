eb0d6ecd4a6f947307885970f7ff0a3fe059d997
Eddier511 <124939498+Eddier511@users.noreply.github.com>
2025-04-11 19:07:14 -0600

    Avance Carrito y facturar.

---
+++ src/main/java/com/tiendaRep/repository/VentaRepository.java
@@ -0,0 +1,8 @@
+package com.tiendaRep.repository;
+
+import com.tiendaRep.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository <Venta,Long> {
+     
+}
